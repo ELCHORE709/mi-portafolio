57ea34251abcdf6bc7b9ad4b1b7f279544cbf2ea
Mauricio Navarro <113221751+ELCHORE709@users.noreply.github.com>
2025-09-14 17:28:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+// Toggle para el menú móvil
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.getElementById('nav-menu').classList.toggle('active');
+});
+
+// Cierra el menú cuando se hace clic en un enlace (en móvil)
+document.querySelectorAll('.nav-links a').forEach(item => {
+    item.addEventListener('click', () => {
+        const navMenu = document.getElementById('nav-menu');
+        if (navMenu.classList.contains('active')) {
+            navMenu.classList.remove('active');
+        }
     });
 });
